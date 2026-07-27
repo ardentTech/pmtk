d02cf3d2970ed54d3f966145b1867492c6277ed8
Jonathan D. Baker <jonathan@ardent.tech>
2026-07-27 13:13:43 -0600

    add clear epo and standby mode commands

---
+++ src/command/clear_epo.rs
@@ -0,0 +1,36 @@
+use crate::error::PmtkError;
+use crate::response::ack::Ack;
+use crate::traits::{Command, Message};
+use crate::types::PmtkPacket;
+
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+#[derive(Debug, Copy, Clone)]
+pub struct ClearEpo;
+
+impl Message for ClearEpo {
+    const PKT_TYPE: u16 = 127;
+}
+
+impl Command for ClearEpo {
+    type Response = Ack;
+
+    fn encode(&self) -> Result<PmtkPacket, PmtkError> {
+        PmtkPacket::new_command(Self::PKT_TYPE, None)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn encode_ok() {
+        let cmd = ClearEpo;
+        let packet = PmtkPacket {
+            checksum: 0x36,
+            data_field: None,
+            pkt_type: ClearEpo::PKT_TYPE,
+        };
+        assert_eq!(packet, cmd.encode().unwrap());
+    }
+}
